50839b8965888d5c6a9c03b01085056beeddeb1e
Tangent-Works <Tangent-Works>
2025-01-17 16:14:36 +0000

    rename example

--- examples/notebooks/Periodic Rebuilding Simulation.py
+++ examples/notebooks/Periodic Rebuilding Simulation.py
@@ -1,6 +1,6 @@
 # Databricks notebook source
 # MAGIC %md
-# MAGIC # Tangent Databricks Example - Forecasting
+# MAGIC # Tangent Databricks Example - Periodic Rebuilding Simulation
 
 # COMMAND ----------
 
